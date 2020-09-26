3db0985b284a1101c1e54b9db3bfc48948a7dfe3
sharadagarwal16 <sharadagarwal16@gmail.com>
2020-09-26 14:25:17 -0400

    02_09 complete

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -5,4 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+}
