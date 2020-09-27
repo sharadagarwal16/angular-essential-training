89abb6a080ba708e133f54e69e91abd158c5f592
sharadagarwal16 <sharadagarwal16@gmail.com>
2020-09-27 12:21:57 -0400

    06_03b complete

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,66 +1,85 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MediaItemService {
-  mediaItems = [
-    {
-      id: 1,
-      name: 'Firebug',
-      medium: 'Series',
-      category: 'Science Fiction',
-      year: 2010,
-      watchedOn: 1294166565384,
-      isFavorite: false
-    },
-    {
-      id: 2,
-      name: 'The Small Tall',
-      medium: 'Movies',
-      category: 'Comedy',
-      year: 2015,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 3,
-      name: 'The Redemption',
-      medium: 'Movies',
-      category: 'Action',
-      year: 2016,
-      watchedOn: null,
-      isFavorite: false
-    }, {
-      id: 4,
-      name: 'Hoopers',
-      medium: 'Series',
-      category: 'Drama',
-      year: null,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 5,
-      name: 'Happy Joe: Cheery Road',
-      medium: 'Movies',
-      category: 'Action',
-      year: 2015,
-      watchedOn: 1457166565384,
-      isFavorite: false
+    mediaItems = [
+        {
+            id: 1,
+            name: 'Firebug',
+            medium: 'Series',
+            category: 'Science Fiction',
+            year: 2010,
+            watchedOn: 1294166565384,
+            isFavorite: false
+        },
+        {
+            id: 2,
+            name: 'The Small Tall',
+            medium: 'Movies',
+            category: 'Comedy',
+            year: 2015,
+            watchedOn: null,
+            isFavorite: true
+        }, {
+            id: 3,
+            name: 'The Redemption',
+            medium: 'Movies',
+            category: 'Action',
+            year: 2016,
+            watchedOn: null,
+            isFavorite: false
+        }, {
+            id: 4,
+            name: 'Hoopers',
+            medium: 'Series',
+            category: 'Drama',
+            year: null,
+            watchedOn: null,
+            isFavorite: true
+        }, {
+            id: 5,
+            name: 'Happy Joe: Cheery Road',
+            medium: 'Movies',
+            category: 'Action',
+            year: 2015,
+            watchedOn: 1457166565384,
+            isFavorite: false
+        }
+    ];
+
+    constructor(private http: HttpClient) { }
+
+    get() {
+        return this.http.get<MediaItemResponse>('mediaitems')
+            .pipe(map(response => response.mediaItems));
     }
-  ];
 
-  get() {
-    return this.mediaItems;
-  }
-
-  add(mediaItem) {
-    this.mediaItems.push(mediaItem);
-  }
-
-  delete(mediaItem) {
-    const index = this.mediaItems.indexOf(mediaItem);
-    if (index >= 0) {
-      this.mediaItems.splice(index, 1);
+    add(mediaItem) {
+        this.mediaItems.push(mediaItem);
     }
-  }
+
+    delete(mediaItem) {
+        const index = this.mediaItems.indexOf(mediaItem);
+        if (index >= 0) {
+            this.mediaItems.splice(index, 1);
+        }
+    }
+}
+
+interface MediaItem {
+    id: number;
+    name: string;
+    medium: string;
+    category: string;
+    year: number;
+    watchedOn: number;
+    isFavorite: boolean;
+}
+
+interface MediaItemResponse {
+    mediaItems: MediaItem[];
 }
